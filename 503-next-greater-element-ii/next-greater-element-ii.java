432902c48747c355f8ec4fc311e9cc6a498344ad
kiruthika-k05 <kiruthika0116@gmail.com>
2026-02-05 10:36:03 +0530

    Time: 15 ms (55.82%) | Memory: 48.9 MB (9.89%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -4,7 +4,7 @@ class Solution {
         int n = nums.length;
         int[] result = new int[n];
         Arrays.fill(result,-1);
-        for(int i = 2*n-1; i >= 0; i--){
+        for(int i = 2*n; i >= 0; i--){
             int idx = i % n;
             while(!st.isEmpty() && st.peek() <= nums[idx]) {
 				st.pop();
